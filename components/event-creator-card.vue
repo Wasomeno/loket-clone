29cab5e3004ad2dcfdf84ed5e27e50d9cfb0d81c
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-30 20:49:07 +0700

    feat(component): add new component for showing event creator

---
+++ components/event-creator-card.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <NuxtLink to="/creator/1">
+    <div class="flex w-40 flex-col items-center justify-center gap-2">
+      <div
+        class="flex h-28 w-28 items-center justify-center rounded-full bg-blue-200 shadow-sm"
+      >
+        <span class="font-semibold">T</span>
+      </div>
+      <span class="font-medium">Event Creator</span>
+    </div>
+  </NuxtLink>
+</template>
