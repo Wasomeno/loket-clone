e98cb3011891fa10a0eb3936ca57686b3f7382a4
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-30 20:49:40 +0700

    feat(component): add new component for showing event details in a card

---
+++ components/event-item-card.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps({
+  eventName: String
+})
+</script>
+
+<template>
+  <RouterLink to="/event/1">
+    <div class="col-span-1 overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+      <div class="h-36 bg-slate-300" />
+      <div class="px-4">
+        <div class="flex flex-col gap-2 py-6">
+          <span class="">Event {{ eventName }}</span>
+          <span class="opacity-50">02 September 2023</span>
+          <span class="font-semibold">Rp 300.000</span>
+        </div>
+        <div class="border-t border-t-slate-300 py-4">
+          <div class="flex items-center gap-4">
+            <div class="h-7 w-7 rounded-full bg-blue-300" />
+            <span class="text-xs font-medium opacity-50">Event Creator</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </RouterLink>
+</template>
+
+<style />
