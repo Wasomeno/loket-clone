9d855356c50c34aa4eb9308931435a6017839f0a
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-30 20:02:04 +0700

    initial commit

---
+++ pages/category/[categoryId].vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="min-h-screen bg-slate-500"></main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,2 @@
+<script setup lang="ts"></script>
+<template>tets</template>
