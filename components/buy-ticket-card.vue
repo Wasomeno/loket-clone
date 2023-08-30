efa9dc0be6cd6300a296a61cdf1914b37d338500
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-30 20:48:36 +0700

    feat(component): add new component for ticket cards in event details page

---
+++ components/buy-ticket-card.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="flex w-full flex-col gap-4 rounded-lg border border-slate-300 bg-blue-50 p-4"
+  >
+    <div class="space-y-2">
+      <div class="text-lg">Ticket Type Title</div>
+      <div class="text-sm tracking-wide">
+        terms of services of the selected ticket ty[e
+      </div>
+      <div class="text-sm font-medium text-blue-600">Ticket sale end time</div>
+    </div>
+    <hr class="w-full rounded-full border-t border-dashed border-slate-300" />
+    <div class="flex items-center justify-between">
+      <span class="text-lg font-medium">Rp 900.000</span>
+      <button
+        class="rounded-lg bg-blue-800 px-4 py-2 text-sm font-medium text-white"
+      >
+        Buy Ticket
+      </button>
+    </div>
+  </div>
+</template>
