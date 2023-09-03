a46802b48f5097987f881d8e4ad6c97df1e43e23
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 18:00:21 +0700

    feat(component): add button component made with class variance authority

---
+++ components/button.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { cn } from "~/lib/utils";
+import { cva } from "class-variance-authority";
+
+const props = withDefaults(
+  defineProps<{ variant: "default" | "defaultOutline" }>(),
+  {
+    variant: "default",
+  },
+);
+
+const buttonVariants = computed(() => {
+  return cva(
+    "flex items-center duration-200 transition disabled:opacity-50 justify-center rounded-lg py-2 px-4",
+    {
+      variants: {
+        intent: {
+          default: "bg-slate-50 hover:bg-slate-100",
+          defaultOutline:
+            "bg-slate-50 hover:bg-slate-100 border border-slate-300",
+        },
+      },
+    },
+  )({ intent: props.variant });
+});
+</script>
+
+<template>
+  <button :class="cn(buttonVariants)">
+    <slot />
+  </button>
+</template>
