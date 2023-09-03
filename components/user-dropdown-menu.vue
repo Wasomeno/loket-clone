384cf0fcfbc61255e5bf078e7f756718d72734ea
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 18:02:14 +0700

    feat(component): add dropdown menu component for user

---
+++ components/user-dropdown-menu.vue
@@ -0,0 +1,126 @@
+<script setup lang="ts">
+import { cilAccountLogout } from "@coreui/icons";
+import { CIcon } from "@coreui/icons-vue";
+
+type ActiveTab = "user" | "creator";
+
+const activeTab = ref<ActiveTab>("user");
+const session = useAuth();
+
+const props = defineProps<{
+  isShowUserMenu: boolean;
+}>();
+
+const emits = defineEmits<{
+  "show-user-menu": [];
+  "hide-user-menu": [];
+}>();
+</script>
+
+<template>
+  <div
+    v-if="session.status.value === 'authenticated'"
+    @mouseenter="$emit('show-user-menu')"
+    @mouseleave="$emit('hide-user-menu')"
+    class="relative flex items-center"
+  >
+    <div
+      class="flex h-10 w-10 items-center justify-center overflow-hidden rounded-full border-2 bg-white shadow-sm"
+    >
+      <img
+        :src="session.data.value?.user?.image as string"
+        alt="user-image"
+        referrerpolicy="no-referrer"
+      />
+    </div>
+    <div
+      v-if="isShowUserMenu"
+      class="absolute right-0 top-12 z-20 flex h-96 w-80 flex-1 flex-col rounded-xl bg-white px-6 py-2 shadow-md"
+    >
+      <div class="flex items-center gap-4 py-4">
+        <div
+          class="flex h-10 w-10 items-center justify-center rounded-full bg-blue-200 shadow-sm"
+        >
+          <img
+            :src="session.data.value?.user?.image as string"
+            referrerpolicy="no-referrer"
+            alt="user-image"
+          />
+        </div>
+        <div>
+          <h5>{{ session.data.value?.user?.name }}</h5>
+          <span class="text-sm font-medium opacity-50">{{
+            session.data.value?.user?.email
+          }}</span>
+        </div>
+      </div>
+      <div class="mb-2 flex items-center">
+        <button
+          @click="activeTab = 'user'"
+          :class="{ 'border-blue-700': activeTab === 'user' }"
+          class="w-full border-b-2 pb-1.5 text-xs font-medium"
+        >
+          User
+        </button>
+        <button
+          @click="activeTab = 'creator'"
+          :class="{ 'border-blue-700': activeTab === 'creator' }"
+          class="w-full border-b-2 pb-1.5 text-xs font-medium"
+        >
+          Creator
+        </button>
+      </div>
+      <div v-if="activeTab === 'creator'" class="flex flex-1 flex-col">
+        <NuxtLink
+          to="#"
+          class="rounded-lg px-4 py-2 transition duration-200 hover:bg-slate-100"
+        >
+          <span class="text-sm">Dashboard</span>
+        </NuxtLink>
+        <NuxtLink
+          to="#"
+          class="rounded-lg px-4 py-2 transition duration-200 hover:bg-slate-100"
+        >
+          <span class="text-sm">My Events</span>
+        </NuxtLink>
+        <NuxtLink
+          to="/create-event"
+          class="rounded-lg px-4 py-2 transition duration-200 hover:bg-slate-100"
+        >
+          <span class="text-sm">Create Event</span>
+        </NuxtLink>
+      </div>
+      <div v-else="activeTab === 'user'" class="flex flex-1 flex-col gap-2">
+        <NuxtLink
+          to="#"
+          class="rounded-lg px-4 py-2 transition duration-200 hover:bg-slate-100"
+        >
+          <span class="text-sm">My Tickets</span>
+        </NuxtLink>
+      </div>
+      <div class="border-t py-2">
+        <button
+          @click="session.signOut"
+          class="flex items-center gap-4 px-4 py-2 text-sm text-red-700"
+        >
+          <CIcon :icon="cilAccountLogout" class="h-4 w-4" />
+          <span>Sign out</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  <div v-else class="flex items-center gap-6">
+    <NuxtLink
+      to="/register"
+      class="rounded-lg border border-slate-50 px-4 py-2 text-sm font-medium text-slate-50"
+    >
+      Register
+    </NuxtLink>
+    <NuxtLink
+      to="/login"
+      class="rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-slate-50"
+    >
+      Login
+    </NuxtLink>
+  </div>
+</template>
