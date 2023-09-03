0fbc4f1a4cbcf42af86a574bd818630ba6f277c1
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 18:18:49 +0700

    refactor: add fetched events data, wrap the page content with nuxt layout and replace dummy datas with real datas

--- pages/discover/index.vue
+++ pages/discover/index.vue
@@ -1,29 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { EVENT_API_MAIN } from "~/lib/utils";
+import { EventType } from "~/types/event";
+
+const { pending, data } = await useFetch<EventType[]>(
+  `${EVENT_API_MAIN}/events`,
+);
+
+const events = toRaw(data.value);
+</script>
 
 <template>
-  <main class="flex min-h-screen flex-1 bg-slate-50">
-    <div class="w-3/12 border-r-2 px-10 py-6">
-      <h3 class="border-b pb-2 text-xl font-semibold tracking-wide">Filter</h3>
-    </div>
-    <div class="w-5/6 px-4">
-      <div class="my-6 flex items-center justify-between">
-        <span class="text-sm">Showing 9 out of 69 Events</span>
-        <button
-          class="w-32 rounded-md border bg-white px-4 py-1.5 text-sm font-medium"
-        >
-          Sort
-        </button>
+  <NuxtLayout>
+    <main class="flex min-h-screen flex-1 bg-slate-50">
+      <div class="w-3/12 border-r-2 px-10 py-6">
+        <h3 class="border-b pb-2 text-xl font-semibold tracking-wide">
+          Filter
+        </h3>
       </div>
-      <div class="grid grid-cols-4 gap-6">
-        <EventItemCard />
-        <EventItemCard />
-        <EventItemCard />
-        <EventItemCard />
-        <EventItemCard />
-        <EventItemCard />
-        <EventItemCard />
-        <EventItemCard />
+      <div class="w-5/6 px-4">
+        <div class="my-6 flex items-center justify-between">
+          <span class="text-sm">Showing 9 out of 69 Events</span>
+          <button
+            class="w-32 rounded-md border bg-white px-4 py-1.5 text-sm font-medium"
+          >
+            Sort
+          </button>
+        </div>
+        <div class="grid grid-cols-4 gap-6">
+          <EventItemCard v-for="event in events" :event-details="event" />
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
+  </NuxtLayout>
 </template>
