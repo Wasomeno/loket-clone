e0b09eca1cbb175b965175899de93c02b8b037f6
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 18:16:15 +0700

    feat(dashboard): add simple dashboard page for user

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "user-dashboard-layout",
+});
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="px-6 py-4">
+      <h1 class="text-xl font-medium">Dashboard</h1>
+    </div>
+  </NuxtLayout>
+</template>
