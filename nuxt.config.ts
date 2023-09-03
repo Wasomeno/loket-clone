3c218f6d7929e6e55c3d2b46cdf66ec507df7bac
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 18:21:49 +0700

    refactor: add properties needed for the app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  runtimeConfig: {
+    public: {
+      NUXT_AUTH_SECRET: "EE2AAE7E145D6B452FF6995D739CA",
+      GOOGLE_CLIENT_ID:
+        "63216408228-82nlub8ge2pht65hc1ph20itkhphq45o.apps.googleusercontent.com",
+      GOOGLE_SECRET: "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",
+    },
+  },
 });
