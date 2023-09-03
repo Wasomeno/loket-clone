eaed46ccacf0e926ffdad313127e7113d61613b0
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 18:01:09 +0700

    feat(component): add footer componet

---
+++ components/footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="flex gap-20 bg-blue-950 p-10 text-slate-50">
+    <div>
+      <span class="text-lg font-semibold tracking-wider text-slate-50"
+        >Loket</span
+      >
+    </div>
+    <div class="grid w-3/6 grid-cols-4">
+      <div>
+        <h4 class="mb-4 font-medium">Footer 1</h4>
+        <ul class="space-y-2">
+          <li class="text-sm opacity-75">List item</li>
+          <li class="text-sm opacity-75">List item</li>
+          <li class="text-sm opacity-75">List item</li>
+        </ul>
+      </div>
+      <div>
+        <h4 class="mb-4 font-medium">Footer 1</h4>
+        <ul class="space-y-2">
+          <li class="text-sm opacity-75">List item</li>
+          <li class="text-sm opacity-75">List item</li>
+          <li class="text-sm opacity-75">List item</li>
+        </ul>
+      </div>
+      <div>
+        <h4 class="mb-4 font-medium">Footer 1</h4>
+        <ul class="space-y-2">
+          <li class="text-sm opacity-75">List item</li>
+          <li class="text-sm opacity-75">List item</li>
+          <li class="text-sm opacity-75">List item</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
