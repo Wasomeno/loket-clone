0fb7f823215d8817cd45e3075037bdd27d89cfc5
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 18:01:20 +0700

    feat(component) : add modal component

---
+++ components/modal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { type ClassValue } from "clsx";
+import { cn } from "../lib/utils";
+
+const props = defineProps<{
+  class: ClassValue;
+  closeModal?: () => void;
+}>();
+</script>
+
+<template>
+  <div
+    class="bl fixed bottom-0 z-30 h-screen w-screen bg-neutral-800 bg-opacity-50 backdrop-blur-[2px]"
+    @click="closeModal"
+  />
+  <div
+    :class="
+      cn(
+        'fixed inset-x-1/2 bottom-0 z-40 h-[90%] w-full -translate-x-1/2 flex-col rounded-none rounded-t-2xl shadow-md lg:inset-y-1/2 lg:-translate-y-1/2 lg:rounded-lg',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
