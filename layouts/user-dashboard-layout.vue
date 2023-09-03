200f682cd23021487528f07316bb98b12a48f6db
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 18:07:26 +0700

    feat(layout): add custom layout for user dashboard

---
+++ layouts/user-dashboard-layout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <UserNavigation />
+    <main class="flex min-h-screen flex-1 bg-slate-50">
+      <UserDashboardNavigation />
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
