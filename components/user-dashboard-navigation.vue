746fa15ebca656d79346b165b99002b57d48d6d8
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-04 21:39:26 +0700

    refactor(component):  move profile link to the main section and add href route for other links

--- components/user-dashboard-navigation.vue
+++ components/user-dashboard-navigation.vue
@@ -13,26 +13,27 @@
           Dashboard
         </div>
       </NuxtLink>
-    </div>
-    <div class="flex flex-col gap-2">
-      <div class="border-b border-gray-300 py-1.5">
-        <span class="mb-2 text-sm font-medium opacity-40">Event Creator</span>
-      </div>
-      <NuxtLink to="/dashboard/creator">
+      <NuxtLink to="/dashboard/profile">
         <div
           class="rounded-md px-4 py-2 font-medium transition duration-200 hover:bg-blue-100"
         >
           Profile
         </div>
       </NuxtLink>
-      <NuxtLink to="/">
+    </div>
+    <div class="flex flex-col gap-2">
+      <div class="border-b border-gray-300 py-1.5">
+        <span class="mb-2 text-sm font-medium opacity-40">Event Creator</span>
+      </div>
+
+      <NuxtLink to="/dashboard/events">
         <div
           class="rounded-md px-4 py-2 font-medium transition duration-200 hover:bg-blue-100"
         >
           My Events
         </div>
       </NuxtLink>
-      <NuxtLink to="/">
+      <NuxtLink to="/create-event">
         <div
           class="rounded-md px-4 py-2 font-medium transition duration-200 hover:bg-blue-100"
         >
@@ -44,7 +45,7 @@
       <div class="border-b border-gray-300 py-1.5">
         <span class="mb-2 text-sm font-medium opacity-40">Tickets</span>
       </div>
-      <NuxtLink to="/">
+      <NuxtLink to="/dashboard/tickets">
         <div
           class="rounded-md px-4 py-2 font-medium transition duration-200 hover:bg-blue-100"
         >
