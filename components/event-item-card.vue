fb144441233580416d7a853593450fc49e9af957
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-11 23:00:42 +0700

    refactor(class):  add tailwind classes for responsiveness

--- components/event-item-card.vue
+++ components/event-item-card.vue
@@ -2,7 +2,7 @@
 import { EventType } from "~/types/event";
 
 const props = defineProps<{ eventDetails: EventType }>();
-const dateTimeStart = new Date(props.eventDetails?.date_time_start);
+const dateTimeStart = new Date(props.eventDetails?.date_time_start as string);
 </script>
 
 <template>
@@ -10,16 +10,18 @@ const dateTimeStart = new Date(props.eventDetails?.date_time_start);
     <div
       class="col-span-1 overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
     >
-      <div class="h-36 bg-slate-300" />
+      <div class="h-24 bg-slate-300 lg:h-36" />
       <div class="px-4">
-        <div class="flex flex-col gap-2 py-6">
-          <span class="">{{ eventDetails.title }}</span>
-          <span class="opacity-50">{{ dateTimeStart.toDateString() }}</span>
-          <span class="font-semibold"
+        <div class="flex flex-col gap-2 py-4 lg:py-6">
+          <span class="text-sm lg:text-base">{{ eventDetails.title }}</span>
+          <span class="text-xs opacity-50 lg:text-base">{{
+            dateTimeStart.toDateString()
+          }}</span>
+          <span class="text-sm font-semibold lg:text-base"
             >Rp {{ eventDetails.min_ticket_price?.toLocaleString("id") }}</span
           >
         </div>
-        <div class="border-t border-t-slate-300 py-4">
+        <div class="border-t border-t-slate-300 py-3 lg:py-4">
           <div class="flex items-center gap-4">
             <div class="h-7 w-7 rounded-full bg-blue-300" />
             <span class="text-xs font-medium opacity-50">{{
