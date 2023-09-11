ac19739a1df64d6bfa10d6769760f48c3f8324cb
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-11 23:19:04 +0700

    refactor: remove variables and imports inside script tag, remove UserDetailsNotCreated component & add UserMobileNavigation component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,10 @@
-<script setup lang="ts">
-import UserDetailsNotCreated from "~/components/modals/user-details-not-created-modal.vue";
-import { EVENT_API_MAIN } from "~/lib/utils";
-
-const session = useAuth();
-const userDetails = useFetch(
-  `${EVENT_API_MAIN}/users/${session.data.value?.user?.email}`,
-);
-
-const isUserDetailsInvalid = userDetails.data === null;
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <UserNavigation />
-    <UserDetailsNotCreated
-      v-if="isUserDetailsInvalid && !$route.path.includes('/dashboard')"
-    />
     <slot />
+    <UserMobileNavigation />
     <Footer />
   </div>
 </template>
