27abfe5dc53284c2a088c6988b9b96a8c4b04418
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-11 23:31:26 +0700

    refactor(class): add tailwind classes for responsiveness

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -23,12 +23,16 @@ function setActiveTab(tab: "phone" | "email") {
 <template>
   <main class="flex min-h-screen flex-1 flex-col gap-6 bg-slate-50">
     <div class="py-4 text-center">
-      <RouterLink to="/" class="text-3xl font-semibold tracking-wider"
+      <RouterLink
+        to="/"
+        class="text-sm font-semibold tracking-wider lg:text-3xl"
         >Loket</RouterLink
       >
     </div>
     <div class="flex flex-1 justify-evenly gap-4">
-      <div class="flex w-3/12 flex-col items-center justify-start gap-6">
+      <div
+        class="hidden w-3/12 flex-col items-center justify-start gap-6 lg:flex"
+      >
         <img
           src="https://img.freepik.com/free-vector/flat-hand-drawn-young-people-using-smartphones_52683-56395.jpg?w=2000&t=st=1693225295~exp=1693225895~hmac=de61bf55b857ae6cec57ffac7ee98f218086bc5d568f580a9a662d6b05d9343a"
           class="w-96 rounded-xl shadow-sm"
@@ -43,11 +47,12 @@ function setActiveTab(tab: "phone" | "email") {
           </p>
         </div>
       </div>
-      <div class="flex w-3/12 flex-col items-center gap-4">
+      <div class="flex w-full flex-col items-center gap-4 p-4 lg:w-3/12">
         <div class="space-y-3 text-center">
-          <h5 class="text-xl font-semibold">Register your account</h5>
-
-          <span class="text-sm"
+          <h5 class="text-lg font-semibold lg:text-xl">
+            Register your account
+          </h5>
+          <span class="text-xs lg:text-sm"
             >Already have an account?
             <RouterLink to="/login"
               ><span class="font-semibold text-blue-800"
@@ -63,14 +68,14 @@ function setActiveTab(tab: "phone" | "email") {
             <button
               :disabled="tabStates.isEmailTab"
               @click="setActiveTab('phone')"
-              class="w-3/6 border-b-2 pb-2 text-center font-medium transition duration-200 disabled:opacity-50"
+              class="w-3/6 border-b-2 pb-2 text-center text-sm font-medium transition duration-200 disabled:opacity-50 lg:text-base"
               :class="{ 'border-b-blue-900': tabStates.isPhoneTab }"
             >
               Phone Number
             </button>
             <button
               @click="setActiveTab('email')"
-              class="w-3/6 border-b-2 pb-2 text-center font-medium transition duration-200"
+              class="w-3/6 border-b-2 pb-2 text-center text-sm font-medium transition duration-200 lg:text-base"
               :class="{ 'border-b-blue-900': tabStates.isEmailTab }"
             >
               Email
@@ -95,7 +100,7 @@ function setActiveTab(tab: "phone" | "email") {
             </div>
           </div>
           <button
-            class="w-full rounded-lg bg-blue-800 p-2 font-medium text-white"
+            class="w-full rounded-lg bg-blue-800 p-2 text-sm font-medium text-white lg:text-base"
           >
             Register
           </button>
@@ -103,12 +108,12 @@ function setActiveTab(tab: "phone" | "email") {
         <div class="flex items-center gap-4">
           <button
             disabled
-            class="w-40 rounded-lg bg-blue-900 p-2 text-sm font-medium text-white disabled:opacity-50"
+            class="w-40 rounded-lg bg-blue-900 p-2 text-xs font-medium text-white disabled:opacity-50 lg:text-sm"
           >
             Facebook
           </button>
           <button
-            class="w-40 rounded-lg border bg-white p-2 text-sm font-medium"
+            class="w-40 rounded-lg border bg-white p-2 text-xs font-medium lg:text-sm"
           >
             Google
           </button>
