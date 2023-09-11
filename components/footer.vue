177285dc45f927ee7411e568246aa9816fcd1cdd
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-11 23:03:28 +0700

    refactor(class): add tailwind class for responsiveness and hide the footer when screen size is lower than large

--- components/footer.vue
+++ components/footer.vue
@@ -1,11 +1,13 @@
 <template>
-  <footer class="flex gap-20 bg-blue-950 p-10 text-slate-50">
+  <footer
+    class="hidden flex-col gap-4 bg-blue-950 p-6 text-slate-50 lg:flex lg:flex-row lg:gap-20 lg:p-10"
+  >
     <div>
       <span class="text-lg font-semibold tracking-wider text-slate-50"
         >Loket</span
       >
     </div>
-    <div class="grid w-3/6 grid-cols-4">
+    <div class="grid w-full grid-flow-col grid-cols-3 lg:w-3/6">
       <div>
         <h4 class="mb-4 font-medium">Footer 1</h4>
         <ul class="space-y-2">
