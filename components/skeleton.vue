6c3636554871f60c0e8dfed8148e847c0c476b30
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-11 22:56:11 +0700

    feat(component): add skeleton component

---
+++ components/skeleton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { cn } from "~/lib/utils";
+import type { ClassValue } from "clsx";
+
+const props = defineProps<{
+  class: ClassValue;
+}>();
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'relative isolate overflow-hidden rounded-lg border bg-gray-300 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-gray-200 before:to-transparent',
+        props.class,
+      )
+    "
+  />
+</template>
