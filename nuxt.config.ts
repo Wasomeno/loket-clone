256477d9d83b9be4122d3c35b8d97149b561c068
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-11 23:49:59 +0700

    refactor(config): add nuxt auth origin property

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
+  auth: { origin: "https://loket-clone.vercel.app/" },
   postcss: {
     plugins: {
       tailwindcss: {},
