1de04535076d40d96b60355fb261fc7e737d4abe
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-11 22:56:50 +0700

    refactor(component): replace displayed elements with skeleton component

--- components/event-item-card-skeleton.vue
+++ components/event-item-card-skeleton.vue
@@ -2,17 +2,17 @@
   <div
     class="col-span-1 overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
   >
-    <div class="h-36 bg-slate-300" />
+    <Skeleton class="h-36 w-full rounded-none" />
     <div class="px-4">
       <div class="flex flex-col gap-2 py-6">
-        <span class="">Event </span>
-        <span class="opacity-50"></span>
-        <span class="font-semibold">Rp 300.000</span>
+        <Skeleton class="h-[18px] w-40 rounded-lg" />
+        <Skeleton class="h-[18px] w-32 rounded-lg" />
+        <Skeleton class="h-[18px] w-40 rounded-lg" />
       </div>
       <div class="border-t border-t-slate-300 py-4">
         <div class="flex items-center gap-4">
-          <div class="h-7 w-7 rounded-full bg-blue-300" />
-          <span class="text-xs font-medium opacity-50">Event Creator</span>
+          <Skeleton class="h-7 w-7 rounded-full" />
+          <Skeleton class="h-[16px] w-32" />
         </div>
       </div>
     </div>
