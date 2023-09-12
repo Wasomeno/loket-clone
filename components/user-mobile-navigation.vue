39b4e0188c2beebde217893ca50e8dae227fa6d9
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-12 23:11:29 +0700

    refactor(navigation): replace dashboard link with discover link, replace dashboard events and tickets with the new links

--- components/user-mobile-navigation.vue
+++ components/user-mobile-navigation.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { cilFlagAlt, cilHome, cilViewQuilt } from "@coreui/icons";
+import { cilCompass, cilFlagAlt, cilHome, cilViewQuilt } from "@coreui/icons";
 import { CIcon } from "@coreui/icons-vue";
 </script>
 
@@ -11,21 +11,34 @@ import { CIcon } from "@coreui/icons-vue";
       <CIcon :icon="cilHome" class="h-4 w-4" />
       <span class="text-xs">Home</span>
     </NuxtLink>
-    <NuxtLink href="/dashboard" class="flex flex-1 flex-col items-center gap-1">
-      <CIcon :icon="cilViewQuilt" class="h-4 w-4" />
-      <span class="text-xs">Dashboard</span>
+    <NuxtLink href="/discover" class="flex flex-1 flex-col items-center gap-1">
+      <CIcon :icon="cilCompass" class="h-4 w-4" />
+      <span class="text-xs">Discover</span>
     </NuxtLink>
     <NuxtLink
-      href="/dashboard/tickets"
+      href="/my-tickets"
       class="flex flex-1 flex-col items-center gap-1"
     >
-      <CIcon :icon="cilHome" class="h-4 w-4" />
+      <svg
+        stroke="currentColor"
+        fill="none"
+        stroke-width="1.5"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+        height="1em"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-4 w-4"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
+        ></path>
+      </svg>
       <span class="text-xs">Tickets</span>
     </NuxtLink>
-    <NuxtLink
-      href="/dashboard/events"
-      class="flex flex-1 flex-col items-center gap-1"
-    >
+    <NuxtLink href="/my-events" class="flex flex-1 flex-col items-center gap-1">
       <CIcon :icon="cilFlagAlt" class="h-4 w-4" />
       <span class="text-xs">Events</span>
     </NuxtLink>
