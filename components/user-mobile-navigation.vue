f8874435b73f4eb39ac1f1d0d21af29d3dd75985
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-16 23:45:08 +0700

    refactor: change parent div shadow box class to custom shadow box class value

--- components/user-mobile-navigation.vue
+++ components/user-mobile-navigation.vue
@@ -6,7 +6,7 @@ import Ticket from "./icons/ticket.vue";
 
 <template>
   <div
-    class="sticky bottom-0 flex h-14 flex-1 items-center gap-4 border-t bg-white px-4 py-2 shadow-md lg:hidden"
+    class="sticky bottom-0 flex h-14 flex-1 items-center gap-4 border-t bg-white px-4 py-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)] lg:hidden"
   >
     <NuxtLink href="/" class="flex flex-1 flex-col items-center gap-1">
       <CIcon :icon="cilHome" class="h-4 w-4" />
