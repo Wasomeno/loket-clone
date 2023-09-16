34f9f0602f7eefc07a7fe7a6330416c2d20e2265
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-16 23:25:03 +0700

    refactor(icon): replace chevron right svg with chevron right icon component

--- components/event-sorter.vue
+++ components/event-sorter.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { sortMap } from "~/lib/utils";
+import ChevronRight from "./icons/chevron-right.vue";
 
 const route = useRoute();
 const router = useRouter();
@@ -34,23 +35,7 @@ onBeforeRouteUpdate(({ query }) => {
       class="flex items-center justify-between gap-2 rounded-lg border border-slate-300 bg-white px-4 py-2 text-start text-xs text-black hover:bg-slate-100 lg:w-52 lg:text-sm"
     >
       {{ activeSort?.title ?? "Select sort" }}
-      <svg
-        stroke="currentColor"
-        fill="currentColor"
-        stroke-width="0"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-        height="1em"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-4 w-4"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
-          clip-rule="evenodd"
-        ></path>
-      </svg>
+      <ChevronRight />
     </button>
     <div
       v-if="isSorterOpen"
