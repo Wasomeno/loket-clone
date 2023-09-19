d696170865d2b383ef22ce4b77abb80882dde01d
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-19 22:29:07 +0700

    refactor: change events continainer layout to flex and add skeleton component for when events still fetching

--- pages/my-events/index.vue
+++ pages/my-events/index.vue
@@ -23,14 +23,18 @@ const { data: events } = useFetch<EventType[]>(
     <main class="flex min-h-screen flex-1 bg-slate-50">
       <div class="flex-1 space-y-4 px-6 py-4">
         <h1 class="text-base font-medium lg:text-xl">My Events</h1>
-        <div class="grid grid-cols-1 lg:grid-cols-5">
+        <div
+          v-if="events"
+          class="flex flex-wrap items-center justify-center gap-4 lg:justify-normal"
+        >
           <NuxtLink
             v-for="event in events"
             :key="event.id"
             :to="`/my-events/${event.id}`"
+            class="w-full lg:w-72"
           >
             <div
-              class="col-span-1 overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+              class="w-full overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
             >
               <img
                 class="h-36 w-full"
@@ -39,7 +43,9 @@ const { data: events } = useFetch<EventType[]>(
               />
               <div class="px-4">
                 <div class="flex flex-col gap-2 py-6">
-                  <span class="text-sm font-medium">{{ event.title }}</span>
+                  <span class="line-clamp-1 text-sm font-medium">{{
+                    event.title
+                  }}</span>
                   <div class="flex items-center gap-3 text-sm">
                     <CIcon :icon="cilCalendar" class="h-4 w-4" />
                     {{
@@ -58,6 +64,27 @@ const { data: events } = useFetch<EventType[]>(
               </div>
             </div>
           </NuxtLink>
+        </div>
+        <div v-else="events" class="flex items-center gap-4">
+          <div
+            v-for="n in 3"
+            class="w-full overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)] lg:w-72"
+          >
+            <Skeleton class="h-36 w-full rounded-none" />
+            <div class="px-4">
+              <div class="flex flex-col gap-2 py-6">
+                <Skeleton class="h-[18px] w-40 rounded-lg" />
+                <Skeleton class="h-[18px] w-32 rounded-lg" />
+                <Skeleton class="h-[18px] w-40 rounded-lg" />
+              </div>
+              <div class="border-t border-t-slate-300 py-4">
+                <div class="flex items-center gap-4">
+                  <Skeleton class="h-7 w-7 rounded-full" />
+                  <Skeleton class="h-[16px] w-32" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
