f3c88587163c05ba729b22d310d19ea73304e8bb
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-19 22:20:50 +0700

    feat(modal): add delete confirmation modal for event

---
+++ components/modals/delete-event-modal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { EVENT_API_MAIN } from "~/lib/utils";
+import axios from "axios";
+
+const props = defineProps<{ eventId: number }>();
+const emits = defineEmits<{ "close-modal": [] }>();
+const route = useRoute();
+
+async function deleteEvent() {
+  await axios.delete(`${EVENT_API_MAIN}/events/${route.params.eventId}`);
+}
+</script>
+
+<template>
+  <Modal
+    :close-modal="() => emits('close-modal')"
+    class="fleoverflow-x-scroll flex h-2/6 flex-1 items-center justify-between gap-4 rounded-lg bg-white p-6 lg:h-72 lg:w-96"
+  >
+    <div class="flex flex-1 items-center">
+      <span class="text-sm lg:text-base">
+        Delete Event with an id of {{ eventId }} ?
+      </span>
+    </div>
+    <div class="flex items-center justify-center gap-4">
+      <Button @click="deleteEvent"> Delete </Button>
+      <Button @click="emits('close-modal')" class="bg-red-800 hover:bg-red-700">
+        Cancel
+      </Button>
+    </div>
+  </Modal>
+</template>
