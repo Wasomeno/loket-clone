42f1fd9a7802e8db3e55d63680a486481f727a61
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-19 22:14:59 +0700

    refactor: replace col-span-1 with w-64, replace placeholder div for event image and event creator logo with nuxt image

--- components/event-item-card.vue
+++ components/event-item-card.vue
@@ -8,12 +8,18 @@ const dateTimeStart = new Date(props.eventDetails?.date_time_start as string);
 <template>
   <NuxtLink :to="`/event/${props.eventDetails.id}`">
     <div
-      class="col-span-1 overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+      class="w-64 overflow-hidden rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
     >
-      <div class="h-24 bg-slate-300 lg:h-36" />
+      <NuxtImg
+        class="h-24 w-full bg-slate-300 lg:h-36"
+        :src="eventDetails.banner_image"
+        alt="event-image"
+      />
       <div class="px-4">
         <div class="flex flex-col gap-2 py-4 lg:py-6">
-          <span class="text-sm lg:text-base">{{ eventDetails.title }}</span>
+          <h5 class="line-clamp-1 text-sm lg:text-base">
+            {{ eventDetails.title }}
+          </h5>
           <span class="text-xs opacity-50 lg:text-base">{{
             dateTimeStart.toDateString()
           }}</span>
@@ -23,7 +29,11 @@ const dateTimeStart = new Date(props.eventDetails?.date_time_start as string);
         </div>
         <div class="border-t border-t-slate-300 py-3 lg:py-4">
           <div class="flex items-center gap-4">
-            <div class="h-7 w-7 rounded-full bg-blue-300" />
+            <div
+              class="h-10 w-10 overflow-hidden rounded-full border-2 bg-blue-300"
+            >
+              <NuxtImg :src="eventDetails.creator?.image_logo" />
+            </div>
             <span class="text-xs font-medium opacity-50">{{
               eventDetails?.creator?.name
             }}</span>
