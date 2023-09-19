d17bf4bb550cbd6004f988b71f3661fddb6ffbba
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-19 22:11:39 +0700

    refactor: add nuxt/image in modules, add image domains properties, add supabase project url and api key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
-  modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
-  auth: { origin: "https://loket-clone.vercel.app/" },
+  modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth", "@nuxt/image"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  image: {
+    domains: ["npqvursxoqbyzwgksvio.supabase.co"],
+  },
   runtimeConfig: {
     public: {
       NUXT_AUTH_SECRET: process.env.NUXT_AUTH_SECRET,
       GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
       GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+      PROJECT_URL: process.env.PROJECT_URL,
+      PROJECT_API_KEY: process.env.PROJECT_API_KEY,
     },
   },
 });
